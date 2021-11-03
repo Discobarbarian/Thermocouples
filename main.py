97ab26135d4ad4200e158a94609befff59d479b6
Alexandr Yefimov <YefimovAlexander@yandex.ru>
2021-11-03 19:46:15 +0700

    added main.py, yet w/o any events handlers

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import design
+from PyQt5 import QtWidgets
+
+
+class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = Application()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
